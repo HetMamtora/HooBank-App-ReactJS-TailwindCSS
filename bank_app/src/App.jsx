62dfadbc9a38115532f354b633dd1c96591b8867
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-28 21:14:02 +0530

    updated

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div></div>
+  </div>
+)
 
 export default App
