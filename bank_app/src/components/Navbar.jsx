ced0794111a525528393f6a31e39ce9bc73642c1
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-28 21:38:13 +0530

    Update Navbar.jsx

--- bank_app/src/components/Navbar.jsx
+++ bank_app/src/components/Navbar.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
 
 const Navbar = () => {
   return (
