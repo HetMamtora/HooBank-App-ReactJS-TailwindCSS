e725215b4ddd4ac31a043c84bfa15468e4347e97
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-28 21:22:57 +0530

    updated

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -18,6 +18,13 @@ const App = () => (
     <div className={`bg-primary $${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         Stats
+        Business
+        Billing
+        CardDeal
+        Testimonials
+        Clients
+        CTA
+        Footer
       </div>
     </div>
 
