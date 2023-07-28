8291ca962fdc42d641f811abe355d051b5bf796d
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-28 21:20:06 +0530

    updated

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -10,7 +10,9 @@ const App = () => (
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
-
+      <div className={`${styles.boxWidth}`}>
+        Hero
+      </div>
     </div>
   </div>
 )
