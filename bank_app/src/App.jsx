6b9f3aa54b251446cebb8ec0acf523dd3eca023b
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-28 21:32:22 +0530

    updated

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -7,27 +7,27 @@ const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        Hero
+      <div /> className={`${styles.boxWidth}`}>
+        <Hero />
       </div>
     </div>
 
     <div className={`bg-primary $${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Stats
-        Business
-        Billing
-        CardDeal
-        Testimonials
-        Clients
-        CTA
-        Footer
-      </div>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      <div />
     </div>
 
   </div>
