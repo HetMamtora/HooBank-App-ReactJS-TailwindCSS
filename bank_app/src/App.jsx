e1bd9809aefb84e9dbba1f1120aad2d3403bee42
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-28 21:30:00 +0530

    updated

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './styles';
 
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
+
 const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
