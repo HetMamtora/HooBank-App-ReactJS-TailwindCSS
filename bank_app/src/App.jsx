169c9a6cbb0afa86ff3cc69235be0ab30ce6d4ed
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-28 21:18:52 +0530

    updated

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from './styles';
 
 const App = () => (
   <div className='bg-primary w-full overflow-hidden'>
-    <div></div>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+
+    </div>
   </div>
 )
 
