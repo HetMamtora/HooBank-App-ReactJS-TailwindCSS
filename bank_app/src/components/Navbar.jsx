bc45abecc2a8622152fbcfe10446d6e615832bd5
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-29 11:14:21 +0530

    updated

--- bank_app/src/components/Navbar.jsx
+++ bank_app/src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
               <li
                 key={nav.id}
                 className={`font-poppins font-normal cursor-pointer text-[16px] 
-                          ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} 
+                          ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} 
                           text-white`}
               >
                 <a href={`#${nav.id}`}>{nav.title}</a>
