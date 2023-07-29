f7b3b0d423a36214d225505c41b12a2f6729e9f7
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-29 15:30:05 +0530

    updated

--- bank_app/src/components/Billing.jsx
+++ bank_app/src/components/Billing.jsx
@@ -4,18 +4,29 @@ import styles, { layout } from '../styles'
 
 const Billing = () => (
     
-  <section id='product' className={layout.sectionReverse}>
+  <section id="product" className={layout.sectionReverse}>
     <div className={layout.sectionImgReverse}>
-      <img src={bill} alt='billing' className='w-[100%] h-[100%] relative z-[5]'/>
+      <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
       <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
       <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
-
-      <div className={layout.sectionInfo}>
-        <h2>Easily control your billing & invoicing.</h2>
-      </div>
-
     </div>
+
+    <div className={layout.sectionInfo}>
+      
+      <h2 className={styles.heading2}>
+        Easily control your <br className="sm:block hidden" /> billing & invoicing
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        An invoice or bill is an important written document that indicates the sale or supply by one business to another business or consumer.
+      </p>
+
+      <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+        <img src={apple} alt='google_play' className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer'/>
+        <img src={google} alt='google_play' className='w-[128px] h-[42px] object-contain cursor-pointer'/>
+      </div>
+    </div>
+
   </section>
 )
 
