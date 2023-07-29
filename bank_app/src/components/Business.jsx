6005a18cf4a7ff0be9383340215ef827d2f3181d
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-29 14:50:40 +0530

    updated

--- bank_app/src/components/Business.jsx
+++ bank_app/src/components/Business.jsx
@@ -5,7 +5,7 @@ import Button from './Button'
 
 const FeatureCard = ({icon, title, content, index}) => (
   <div className={`flex flex-row p-6 rounded-[20px] ${index != features.length-1 ? 'mb-6' : 'mb-0'} feature-cad`}>
-    <div>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
       <img src={icon} alt='icon'className='w-[50%] h-[50%] object-contain'/>
     </div>
   </div>
