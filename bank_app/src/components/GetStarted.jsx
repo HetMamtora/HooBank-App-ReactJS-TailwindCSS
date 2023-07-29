1ed8820694c59769ae90ac7ecb3c4cf55fb3c544
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-29 12:28:49 +0530

    updated

--- bank_app/src/components/GetStarted.jsx
+++ bank_app/src/components/GetStarted.jsx
@@ -9,8 +9,12 @@ const GetStarted = () => (
       <div className={`${styles.flexStart} flex-row`}>
         <p className='font-poppins font-medium text-[18px] leading-[23px]'>
           <span className='text-gradient'>Get</span>
+          <img src={arrowUp} className='w-[23px] h-[23px] object-contain'/>
         </p>
       </div>
+      <p className='font-poppins font-medium text-[18px] leading-[23px]'>
+          <span className='text-gradient'>Started</span>
+        </p>
     </div>
   </div>
 )
