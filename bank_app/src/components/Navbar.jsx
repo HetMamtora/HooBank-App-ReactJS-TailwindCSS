f70ec92d7ee1af74fa3547bbf66a4ea3a92fe76a
HetMamtora <104263376+HetMamtora@users.noreply.github.com>
2023-07-29 11:13:21 +0530

    updated

--- bank_app/src/components/Navbar.jsx
+++ bank_app/src/components/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
         <div
           className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <ul className='list-none flex justify-end items-center flex-1'>
+          <ul className='list-none flex-col justify-end items-center flex-1'>
             {navLinks.map((nav,index) => (
               <li
                 key={nav.id}
